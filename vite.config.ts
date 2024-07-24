291c861b7006490026aeef8a26fd54112a0408db
Андрей Епифанов <epifanovmd@gmail.com>
2024-07-25 00:08:00 +0300

    refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import path from "path";
 import ssr from "vike/plugin";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 // @ts-ignore
 import { cjsInterop } from "vite-plugin-cjs-interop";
 
